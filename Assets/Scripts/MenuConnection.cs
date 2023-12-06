91cb2b51eab3174b5a4cdcc77f2a4e196b06d576
mariana maysa <marianamaysaestudos@gmail.com>
2023-12-06 16:57:23 -0300

    telas + wallrunning + velocity player + run animation
    aprimoramento do wallrunning
    correção da velocidade do jogador
    implementação da animação de run
    implementação das imagens

--- Assets/Scripts/MenuConnection.cs
+++ Assets/Scripts/MenuConnection.cs
@@ -5,13 +5,16 @@ using Unity.Netcode;
 
 public class MenuConnection : MonoBehaviour
 {
+    [SerializeField] public GameObject bgMenu;
     public void ClientConnect()
     {
         NetworkManager.Singleton.StartClient();
+        bgMenu.SetActive(false);
     }
 
     public void HostConnect()
     {
         NetworkManager.Singleton.StartHost();
-    }
+        bgMenu.SetActive(false);
+    }    
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,6 +26,7 @@ public class PlayerMovement : NetworkBehaviour
     public float playerSpeed;
     public float jumpHeight;
     public float gravity;
+    [SerializeField] float velocity;
 
     [Header("Lanes")]
     [SerializeField] float laneDistance = 4; //distancia entre duas lanes
@@ -57,29 +58,29 @@ public class PlayerMovement : NetworkBehaviour
         }
         if (Input.GetKey(KeyCode.UpArrow) && (desiredLane == -1 || desiredLane == 3))
         {
-            desiredHeight += Time.deltaTime * 2f;
+            desiredHeight = Mathf.Max(desiredHeight + 1, 5);
         }
         if (Input.GetKey(KeyCode.DownArrow) && (desiredLane == -1 || desiredLane == 3))
         {
-            desiredHeight -= Time.deltaTime * 2f;
+            desiredHeight = Mathf.Min(desiredHeight - 1, -2);
         }
 
-        desiredHeight = Mathf.Clamp(desiredHeight, 2f, 10f);
+        desiredHeight = Mathf.Clamp(desiredHeight, 1f, 9f);
         if (desiredLane >= 0 && desiredLane <= 2)
         {
-            desiredHeight = 2f;
-            Vector3 targetPosition = new Vector3((desiredLane - 1) * laneDistance, 0, transform.position.z + (playerSpeed * Time.deltaTime));
+            desiredHeight = 1f;
+            Vector3 targetPosition = new Vector3((desiredLane - 1) * laneDistance, 0, transform.position.z + (playerSpeed * velocity));
             transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
 
         }
         else if (desiredLane == -1)
         {
-            Vector3 targetPosition = new Vector3(-1.70f * laneDistance, desiredHeight, transform.position.z + (playerSpeed * Time.deltaTime));
+            Vector3 targetPosition = new Vector3(-1.70f * laneDistance, desiredHeight, transform.position.z + (playerSpeed * velocity));
             transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
         }
         else if (desiredLane == 3)
         {
-            Vector3 targetPosition = new Vector3(1.70f * laneDistance, desiredHeight, transform.position.z + (playerSpeed * Time.deltaTime));
+            Vector3 targetPosition = new Vector3(1.70f * laneDistance, desiredHeight, transform.position.z + (playerSpeed * velocity));
             transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
         }
     }
